ef853ba4eb82bd795ab71a8e4b737977b4665e8d
Bob Habig <rfhab2@gmail.com>
2016-09-08 21:14:24 +0000

    successfully found entertainment selectors for :content, :url, :title

--- lib/news_stand/scraper.rb
+++ lib/news_stand/scraper.rb
@@ -1,37 +1,32 @@
 class NewsStand::Scraper
-  def self.scrape_news_page
-    doc = Nokogiri::HTML(open("https://news.google.com/"))
-    counter = 1
-    doc.css("div.topic-list div.topic").each do |category|
-      binding.pry
-      puts "#{counter}. #{category.text}"
-      counter += 1
-    end
-    counter
+
+  def self.scrape_entertainment_page
+    doc = Nokogiri::HTML(open("http://www.cnn.com/entertainment"))
+    #headlines (iterate w/ each to create numbered list and also assign object titles) = doc.css("h3.cd__headline[data-analytics='Top Stories_list-small-vertical_article_'] a span.cd__headline-text")
+
+    #url (iterate w/ each)= doc.css("h3.cd__headline[data-analytics='Top Stories_list-small-vertical_article_'] a") **.each do ** block(puts t.attribute("href").value) ****urls that are generated need the http://www.cnn.com prefix. find a way to add
+
+    #content = doc = Nokogiri::HTML(open("http://www.cnn.com/2016/09/08/entertainment/kanye-west-yeezy-fashion-show/index.html"))
+    #doc.css("div.l-container .zn-body__paragraph").text
   end
 
-  def self.scrape_science_page
-    doc = Nokogiri::HTML(open(@science_page))
-
-  end
-anchorman-two-browse-nav
   def self.scrape_us_page
-    doc = Nokogiri::HTML(open(@us_page))
-
+    doc = Nokogiri::HTML(open("http://www.cnn.com/specials/last-50-stories"))
+    binding.pry
   end
 
   def self.scrape_world_page
-    doc = Nokogiri::HTML(open("https://www.yahoo.com/news/world"))
+    doc = Nokogiri::HTML(open("http://www.cnn.com/world"))
 
   end
 
-  def self.scrape_tech_page
-    doc = Nokogiri::HTML(open("https://www.yahoo.com/tech"))
+  def self.scrape_health_page
+    doc = Nokogiri::HTML(open("http://www.cnn.com/health"))
 
   end
 
-  def self.scrape_odd_page
-    doc = Nokogiri::HTML(open("https://www.yahoo.com/news/odd"))
+  def self.scrape_sports_page
+    doc = Nokogiri::HTML(open("http://bleacherreport.com/trending"))
 
   end
 end
