1517c519214799a2204a5b3d74a2e30f7671723e
Bob Habig <rfhab2@gmail.com>
2016-09-09 02:42:52 +0000

    all selectors but sports are working. make sure to avoid creating objects with video content only

--- lib/news_stand/scraper.rb
+++ lib/news_stand/scraper.rb
@@ -26,17 +26,24 @@ class NewsStand::Scraper
 
   end
 
-  def self.scrape_world
+  def self.scrape_extreme_weather
     doc = Nokogiri::HTML(open("http://www.cnn.com/specials/us/extreme-weather"))
+    #title = doc.css("h3.cd__headline a span.cd__headline-text").each{|x| puts x.text}
+    binding.pry
+    #link: doc.css("h3.cd__headline a").each do |x|
+    #   @link = "http://cnn.com"+x.attribute("href").value
+    #   @article = Nokogiri::HTML(open("#{@link}"))
+    #end
 
+    #content: @article.css("div.l-container .zn-body__paragraph").text ** careful of creating objects without content (some are video)
   end
 
-  def self.scrape_health_page
+  def self.scrape_space_science
     doc = Nokogiri::HTML(open("http://www.cnn.com/specials/space-science"))
 
   end
 
-  def self.scrape_sports_page
+  def self.scrape_sports
     doc = Nokogiri::HTML(open("http://bleacherreport.com/trending"))
 
   end
